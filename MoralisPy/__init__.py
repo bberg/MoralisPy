1027007310364bed61d12802f10086a7d5f5347a
bberg <benberg00@gmail.com>
2021-12-27 17:07:18 -0500

    Add version 1.0.0 version number.

--- MoralisPy/__init__.py
+++ MoralisPy/__init__.py
@@ -1,1 +1,6 @@
+# __init__.py
+
+# Version of the realpython-reader package
+__version__ = "1.0.0"
+
 from MoralisPy.api import MoralisPy
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MoralisPy',
-    version='1.0',
+    version='1.0.0',
     packages=find_packages(exclude=['tests*']),
     license='GNU General Public License v3.0',
     description='A python wrapper for the Molaris REST API',
@@ -10,5 +10,6 @@ setup(
     install_requires=[],
     url='https://github.com/bberg/MoralisPy',
     author='Ben Berg',
-    author_email='benberg00@gmail.com'
+    author_email='benberg00@gmail.com',
+    long_description_content_type='text/markdown'
 )
