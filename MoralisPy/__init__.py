46388609d9b0dfebeacad22493725a1023daea49
bberg <benberg00@gmail.com>
2022-01-02 11:22:20 -0500

    Added requests dependency.

--- MoralisPy/__init__.py
+++ MoralisPy/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 
 # Version of the realpython-reader package
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from MoralisPy.api import MoralisPy
--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='MoralisPy',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=['tests*']),
     license='GNU General Public License v3.0',
     description='A python wrapper for the Molaris REST API',
     long_description=open('README.md').read(),
-    install_requires=[],
+    install_requires=[
+        'requests',
+    ],
     url='https://github.com/bberg/MoralisPy',
     author='Ben Berg',
     author_email='benberg00@gmail.com',
